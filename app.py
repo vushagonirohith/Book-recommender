659905aacf70495704496ab1cefad9bd348bad39
ROHITH VUSHAGONI <vushagonirohith123@gmail.com>
2025-07-19 11:53:51 +0530

    e

--- app.py
+++ app.py
@@ -40,4 +40,5 @@ def recommend():
     return render_template('results.html', books=recommended_books)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000, debug=True)
+
